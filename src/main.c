51c42d7534c9d22b05a1b8821af57bb125d8454d
Just <37525404+dedrend@users.noreply.github.com>
2020-08-28 21:39:08 -0700

    Basic input system

--- src/main.c
+++ src/main.c
@@ -8,15 +8,43 @@
 #endif
 #include <cglm/cglm.h>
 #include <math.h>
-
+#include <string.h>
 #include <time.h>
 
 #define WINDOW_TITLE "Minecraft Clone"
 
 static int width = 1920, height = 1080;
 
+static char key_states[256];
+static void KeyboardEvent(unsigned char key, char is_pressed)
+{
+    key_states[key] = is_pressed;
+}
+
+static void KeyboardDown(unsigned char button, int x, int y)
+{
+    if(button >= 'a' && button <= 'z')
+        button -= 'a' - 'A';
+    KeyboardEvent(button, 1);
+}
+
+static void KeyboardUp(unsigned char button, int x, int y)
+{
+    if(button >= 'a' && button <= 'z')
+        button -= 'a' - 'A';
+    KeyboardEvent(button, 0);
+}
+
+static char get_key_state(unsigned char key)
+{
+    if(key >= 'a' && key <= 'z')
+        key -= 'a' - 'A';
+    return key_states[key];
+}
+
 static void setup()
 {
+    memset(key_states, 0, sizeof(key_states));
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     glFrontFace(GL_CCW);
@@ -33,28 +61,7 @@ static void mouse_motion(int x, int y)
 
 }
 
-static void display(void)
-{
-    glClearColor(1.0f, 0.2f, 0.7f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    
-
-    glutSwapBuffers();
-    glutPostRedisplay();
-}
-
-static void keyboard(unsigned char button, int x, int y)
-{
-
-}
-
-static void keyboard_up(unsigned char button, int x, int y)
-{
-
-}
-
-static void reshape(int w, int h)
+static void Resize(int w, int h)
 {
     width = w > 1 ? w : 1;
     height = h > 1 ? h : 1;
@@ -63,6 +70,15 @@ static void reshape(int w, int h)
     glClearDepth(1.0);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glEnable(GL_DEPTH_TEST);
+}
+
+static void Render(void)
+{
+    glClearColor(0.0f, 0.2f, 0.7f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glutSwapBuffers();
+    glutPostRedisplay();
 }
 
 int main(int argc, char **argv)
@@ -78,10 +94,10 @@ int main(int argc, char **argv)
     glutInitWindowSize(width, height);
     glutCreateWindow(WINDOW_TITLE);
 
-    glutReshapeFunc(reshape);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(keyboard);
-    glutKeyboardUpFunc(keyboard_up);
+    glutReshapeFunc(Resize);
+    glutDisplayFunc(Render);
+    glutKeyboardFunc(KeyboardDown);
+    glutKeyboardUpFunc(KeyboardUp);
     glutPassiveMotionFunc(mouse_motion);
     glutMotionFunc(mouse_motion);
 
